bc51b56f5fcd74abf670dacc26fe24761ac28956
shribhat23 <pranavbhat232002@gmail.com>
2025-09-23 13:57:38 +0530

    initial commit

---
+++ src/test/java/com/pranav/GamingClub/GamingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.GamingClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
